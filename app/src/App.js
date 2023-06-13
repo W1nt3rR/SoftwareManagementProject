460ecc65eb71f56ae53a43a00017ba3005d7b1a5
popovicmarko <popovicmarko350@gmail.com>
2023-06-13 20:28:41 +0200

    navBar finished

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import React from 'react';
 import Heder from './components/Heder/Heder';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <div className="navBar">
+        <NavBar />
+      </div>
       <Heder />
     </div>
   );
